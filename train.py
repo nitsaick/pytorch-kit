101b32414397c81a74e4f510dff7cd84d007966a
Nick Tsai <nick850810@gmail.com>
2019-02-21 17:07:23 +0800

    Add model weight initialize function

--- train.py
+++ train.py
@@ -6,14 +6,15 @@ from argparse import ArgumentParser
 import torch
 from ruamel import yaml
 from tensorboardX import SummaryWriter
+from tqdm import tqdm
 
 import utils.checkpoint as cp
+from network.init_weights import init_weights
 from utils.cfg_reader import *
+from utils.func import count_params
 from utils.metrics import Evaluator
 from utils.vis import imshow
-from utils.func import count_params
 
-from tqdm import tqdm
 
 class Trainer:
     def __init__(self, net, dataset, optimizer, scheduler, criterion, log_dir, cfg):
@@ -23,7 +24,7 @@ class Trainer:
         self.scheduler = scheduler
         self.criterion = criterion
         self.log_dir = log_dir
-        
+
         self.checkpoint_dir = os.path.join(log_dir, 'checkpoint')
         self.epoch_num = cfg['training']['epoch']
         self.start_epoch = 0
@@ -33,21 +34,21 @@ class Trainer:
         self.visualize_iter_interval = cfg['training']['visualize_iter_interval']
         self.num_workers = cfg['training']['num_workers']
         self.eval_func = cfg['training']['eval_func']
-        
+
         self.cuda = cfg['training']['device']['name'] == 'gpu'
         if self.cuda:
             self.gpu_ids = cfg['training']['device']['ids']
-        
+
         self.logger = SummaryWriter(log_dir)
-        
+
         if not os.path.exists(self.checkpoint_dir):
             os.makedirs(self.checkpoint_dir)
-    
+
     def run(self):
         train_loader, valid_loader, _ = self.dataset.get_dataloader(self.batch_size, self.num_workers)
-        
+
         print('{:-^40s}'.format(' Start training '))
-        
+
         if self.cuda:
             device = 'cuda' + str(self.gpu_ids)
         else:
@@ -76,49 +77,49 @@ class Trainer:
             self.logger.add_graph(net, dummy_input)
         except RuntimeError:
             print('Warning: Cannot export net to ONNX, ignore log graph')
-        
+
         for batch_idx, (imgs, labels) in enumerate(valid_loader):
             imgs, labels, _ = self.dataset.vis_transform(imgs, labels, None)
             self.logger.add_images('image', imgs, 0)
             self.logger.add_images('label', labels, 0)
             break
-        
+
         if self.cuda:
             self.net = torch.nn.DataParallel(self.net, device_ids=self.gpu_ids).cuda()
             self.criterion = self.criterion.cuda()
-        
+
         valid_acc = 0.0
         best_acc = 0.0
-        
+
         for epoch in range(self.start_epoch, self.epoch_num):
             epoch_str = ' Epoch {}/{} '.format(epoch + 1, self.epoch_num)
             print('{:-^40s}'.format(epoch_str))
-            
+
             for param_group in self.optimizer.param_groups:
                 lr = param_group['lr']
                 break
             print('Learning rate: {}'.format(lr))
             self.logger.add_scalar('lr', lr, epoch)
-            
+
             # Training phase
             self.net.train()
             torch.set_grad_enabled(True)
             loss = self.training(train_loader)
             self.logger.add_scalar('loss', loss, epoch)
-            
+
             if (epoch + 1) % self.eval_epoch_interval == 0:
                 self.net.eval()
                 torch.set_grad_enabled(False)
-                
+
                 # Evaluation phase
                 train_acc = self.evaluation(train_loader, epoch)
-                
+
                 # Validation phase
                 valid_acc = self.validation(valid_loader, epoch)
 
                 print('Train data {} acc:  {:.5f}'.format(self.eval_func, train_acc))
                 print('Valid data {} acc:  {:.5f}'.format(self.eval_func, valid_acc))
-            
+
             if valid_acc > best_acc:
                 best_acc = valid_acc
                 best_epoch = epoch
@@ -127,25 +128,25 @@ class Trainer:
                 print('Update best acc!')
                 self.logger.add_scalar('best epoch', best_epoch, 0)
                 self.logger.add_scalar('best acc', best_acc, 0)
-            
+
             if (epoch + 1) % self.checkpoint_epoch_interval == 0:
                 checkpoint_filename = 'cp_{:03d}.pth'.format(epoch + 1)
                 cp.save(epoch, self.net.module, self.optimizer, os.path.join(self.checkpoint_dir, checkpoint_filename))
-    
+
     def training(self, train_loader):
         tbar = tqdm(train_loader, ascii=True, desc='train')
         for batch_idx, (imgs, labels) in enumerate(tbar):
             self.optimizer.zero_grad()
-            
+
             if self.cuda:
                 imgs, labels = imgs.cuda(), labels.cuda()
-            
+
             outputs = self.net(imgs)
-            
+
             loss = self.criterion(outputs, labels)
             loss.backward()
             self.optimizer.step()
-            
+
             if batch_idx % 20 == 0:
                 outputs = outputs.cpu().detach().numpy().argmax(axis=1)
                 imgs, labels, outputs = self.dataset.vis_transform(imgs, labels, outputs)
@@ -154,45 +155,45 @@ class Trainer:
 
             tbar.set_postfix(loss='{:.5f}'.format(loss.item()))
         self.scheduler.step(loss.item())
-        
+
         return loss.item()
-    
+
     def evaluation(self, train_loader, epoch):
         evaluator = Evaluator(self.dataset.num_classes)
         tbar = tqdm(train_loader, desc='eval ', ascii=True)
         for batch_idx, (imgs, labels) in enumerate(tbar):
             if self.cuda:
                 imgs = imgs.cuda()
-            
+
             outputs = self.net(imgs)
-            
+
             outputs = outputs.cpu().detach().numpy().argmax(axis=1)
             labels = labels.cpu().detach().numpy()
             evaluator.add_batch(outputs, labels)
-        
+
         train_acc = evaluator.eval(self.eval_func)
         evaluator.log_acc(self.logger, epoch, 'train/')
         return train_acc
-    
+
     def validation(self, valid_loader, epoch):
         evaluator = Evaluator(self.dataset.num_classes)
         tbar = tqdm(valid_loader, desc='valid', ascii=True)
         for batch_idx, (imgs, labels) in enumerate(tbar):
             if self.cuda:
                 imgs = imgs.cuda()
-            
+
             outputs = self.net(imgs)
-            
+
             np_outputs = outputs.cpu().detach().numpy().argmax(axis=1)
             np_labels = labels.cpu().detach().numpy()
             evaluator.add_batch(np_outputs, np_labels)
-            
+
             if batch_idx == 0:
                 vis_imgs, vis_labels, vis_outputs = self.dataset.vis_transform(imgs, labels, outputs)
                 imshow(title='Valid', imgs=(vis_imgs[0], vis_labels[0], vis_outputs[0]), shape=(1, 3),
                        subtitle=('image', 'label', 'predict'))
                 self.logger.add_images('output', vis_outputs, epoch)
-        
+
         valid_acc = evaluator.eval(self.eval_func)
         evaluator.log_acc(self.logger, epoch, 'valid/')
         return valid_acc
@@ -211,31 +212,33 @@ if __name__ == '__main__':
     cfg_file = os.path.expanduser(args.config)
     with open(cfg_file) as fp:
         cfg = yaml.load(fp, Loader=yaml.RoundTripLoader)
-    
+
     cfg_check(cfg)
     print('load config: {}'.format(cfg_file))
 
     path_file = './configs/path.yml'
     with open(path_file) as fp:
         path_cfg = yaml.load(fp, Loader=yaml.Loader)
-    
+
     gpu_check(cfg)
     log_name_check(cfg)
     log_dir = os.path.expanduser(os.path.join(path_cfg['log_root'], cfg['training']['log_name']))
-    
+
     dataset_root = os.path.expanduser(path_cfg[cfg['dataset']['name'].lower()])
     train_transform = get_transform(cfg, 'train')
     valid_transform = get_transform(cfg, 'valid')
-    
+
     dataset = get_dataset(cfg, dataset_root, train_transform, valid_transform)
     criterion = get_criterion(cfg, dataset)
     net = get_net(cfg, dataset)
     optimizer = get_optimizer(cfg, net)
     scheduler = get_scheduler(cfg, optimizer)
-    
+
     eval_func_check(cfg, dataset)
-    
-    
+
+    init_weights(net, cfg['training']['init_weight_func'])
+
+
     def check(path):
         yes = {'yes', 'y', 'ye', ''}
         no = {'no', 'n'}
@@ -246,14 +249,15 @@ if __name__ == '__main__':
                 try:
                     if os.path.isfile(file_path):
                         os.unlink(file_path)
-                    elif os.path.isdir(file_path): shutil.rmtree(file_path)
+                    elif os.path.isdir(file_path):
+                        shutil.rmtree(file_path)
                 except Exception as e:
                     print(e)
             return True
         elif choice in no:
             return False
-    
-    
+
+
     if os.path.exists(log_dir):
         files = os.listdir(log_dir)
         if len(files) >= 3:
